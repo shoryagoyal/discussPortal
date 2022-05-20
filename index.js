4fc8d14f29bf05102472c77e166f86c69ab76d34
shorya_goyal <shoryagoyal18@gmail.com>
2022-05-20 08:24:29 +0530

    Client side validation added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const expressSession = require('express-session');
 const methodOverride = require('method-override');
 const { Console } = require('console');
 const app = express();
-const dbUrl = 'mongodb://localhost:27017/discussPortal'; //process.env.DB_URL;
+const dbUrl = process.env.DB_URL;// 'mongodb://localhost:27017/discussPortal'; //process.env.DB_URL;
 const port = process.env.PORT || 3000;
 
 let errorMessage = false;
